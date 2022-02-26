28b0435bd455022472746bb9a09a6412996c8924
Uladzimizh <99535893+Uladzimizh@users.noreply.github.com>
2022-02-26 13:27:20 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 555;
+        if (a % 10 == 7) {
+            System.out.println("оканчивается");
+        } else {
+            System.out.println("не оканчивается");
+        }
+    }
+}
